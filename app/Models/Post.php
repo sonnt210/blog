f32a9f0d2b0ee3e34a37052d0b07494cc2b5f7e2
sonnt210 <sonnt.code@gmail.com>
2023-12-26 23:42:57 +0700

    Build layout for blog page

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,10 +5,20 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 
     public function scopePublished($query)
     {
@@ -18,5 +28,17 @@ class Post extends Model
     public function scopeFeatured($query)
     {
         $query->where('featured', true);
+    }
+
+    public function getExcerpt()
+    {
+        return Str::limit(strip_tags($this->body), 150);
+    }
+
+    public function getReadingTime()
+    {
+        $words   = str_word_count($this->body);
+        $minutes = round($words / 250);
+        return $minutes < 1 ? 1 : $minutes;
     }
 }
