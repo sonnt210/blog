b90f6eed11027daec0a9d491fa519ddad4b7eae4
sonnt210 <sonnt.code@gmail.com>
2024-01-02 22:47:17 +0700

    add alpinejs search box

--- resources/views/posts/partials/search-box.blade.php
+++ resources/views/posts/partials/search-box.blade.php
@@ -1,4 +1,6 @@
-<div id="search-box">
+<div x-data="{
+    query: '{{ request('search', '') }}'
+}" id="search-box">
     <div>
         <h3 class="text-lg font-semibold text-gray-900 mb-3">Search</h3>
         <div class="w-52 flex rounded-2xl bg-gray-100 py-2 px-3 mb-3 items-center">
@@ -8,10 +10,12 @@
                               d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
                     </svg>
                 </span>
-            <input wire:model="search"
+            <input x-model="query"
                 class="w-40 ml-1 bg-transparent focus:outline-none focus:border-none focus:ring-0 outline-none border-none text-xs text-gray-800 placeholder:text-gray-400"
                 type="text" placeholder="Search Something...">
         </div>
-        <x-button wire:click="update">Search</x-button>
+        <x-button x-on:click="$dispatch('search', {
+            search: query
+        })">Search</x-button>
     </div>
 </div>
