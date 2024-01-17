5abde7575f5de5425477c8833e0a46a04d898ba8
sonnt210 <sonnt.code@gmail.com>
2024-01-17 22:22:16 +0700

    days5: create layout for view post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,4 +16,11 @@ class PostController extends Controller
             })->take(10)->get()
         ]);
     }
+
+    public function show(Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post
+        ]);
+    }
 }
