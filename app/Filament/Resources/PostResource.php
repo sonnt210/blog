a386e3a23e69c998c539ccc1b44b74876828fd02
sonnt210 <sonnt.code@gmail.com>
2024-08-25 20:05:57 +0700

    day9: manager comments in admin

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -96,7 +96,7 @@ class PostResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\CommentsRelationManager::class
         ];
     }
 
