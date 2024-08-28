0c6a6a77c36e0e194d74e73d576debab4254528b
sonnt210 <sonnt.code@gmail.com>
2024-08-28 21:32:19 +0700

    optimize performance with laravel caching

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,16 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Post;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index', [
-            'categories' => Category::whereHas('posts', function ($query) {
+        $categories = Cache::remember('categories', Carbon::now()->addDay(), function () {
+            return Category::whereHas('posts', function ($query) {
                 $query->published();
-            })->take(10)->get()
+            })->take(10)->get();
+        });
+        return view('posts.index', [
+            'categories' => $categories
         ]);
     }
 
